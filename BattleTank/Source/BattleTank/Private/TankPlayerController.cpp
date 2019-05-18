6766ffb97ecb0ae424c49b4ea9bdba2e49cda1ad
lmalfonzo <lmalfonzo@dons.usfca.edu>
2019-05-18 10:35:54 -0700

    :crab: commiting every lecture #161

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -2,7 +2,23 @@
 
 #include "TankPlayerController.h"
 
-ATank* ATankPlayerController::GetControlledTank() const {
+void ATankPlayerController::BeginPlay() 
+{
+	Super::BeginPlay();
+
+	auto PossessedTank = GetControlledTank();
+	if (!PossessedTank) {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not posessing a tank"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("Possessed Tank: %s"), *(PossessedTank->GetName()));
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
+}
+
+ATank* ATankPlayerController::GetControlledTank() const 
+{
 	return Cast<ATank>(GetPawn());
 }
 
