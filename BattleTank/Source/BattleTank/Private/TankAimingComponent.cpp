7c25b8bf63e43a1a790de42681b9ef02ed001ddb
lmalfonzo <lmalfonzo@dons.usfca.edu>
2019-05-22 15:09:03 -0700

    :crab: SUGGEST PROJECTILE BUGGED

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -52,6 +52,9 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 		StartLocation,
 		HitLocation,
 		LaunchSpeed,
+		false,
+		0,
+		0,
 		ESuggestProjVelocityTraceOption::DoNotTrace
 	);
 
