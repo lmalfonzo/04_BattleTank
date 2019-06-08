40126d189d2a7e2efbaec2f54b7e4081d35d611d
lmalfonzo <lmalfonzo@dons.usfca.edu>
2019-06-08 13:17:24 -0700

    :crab: router

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -35,6 +35,7 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
+	UPROPERTY(BlueprintReadOnly, Category = "State")
 	EFiringStatus FiringStats = EFiringStatus::Reloading;
 
 private:	
